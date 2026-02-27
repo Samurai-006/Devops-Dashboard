9324610add99d4fcc39563e0894b6df81e42d4c4
Samurai-006 <harishtamil24@gmail.com>
2026-02-27 07:05:22 +0000

    Made server.js to be runtime

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,3 @@
-const express=require("express");
-const cors=require("cors");
-const { request } = require("node:http");
-
-const app=express();
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/',(req,res)=>{
-    res.send("Devops Dashboard Backend is running");
-});
-
-app.get('/deployments',(req,res)=>{
-    res.send([
-        {status:"Success", time:"2 mins"},
-        {status:"Failed", time:"5 mins"}
-    ]);
-});
-
 const app = require("./app");
 
 app.listen(5000, () => {
